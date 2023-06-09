ec51c609d37719c2cadd8b3f913b1e4309bf95d4
MadsJeppesen <mads.jeppesen@biochemistry.lu.se>
2023-06-09 11:21:35 +0200

    forgot paths.py

---
+++ cubicsym/paths.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Paths to different databases + IP addres to server
+@Author: Mads Jeppesen
+@Date: 4/7/22
+"""
+from pathlib import Path
+DATA = Path(".").resolve().parent.joinpath("data")
