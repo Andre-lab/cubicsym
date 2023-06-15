2e825c40e539227e036c6d6036308e51abb63d7d
MadsJeppesen <mads.jeppesen@biochemistry.lu.se>
2023-06-15 15:51:16 +0200

    removed dependcies on shapedesign

--- cubicsym/actors/cubicsymmetryslider.py
+++ cubicsym/actors/cubicsymmetryslider.py
@@ -9,11 +9,8 @@ import time
 from pyrosetta.rosetta.core.pose.symmetry import is_symmetric
 import random
 from cloudcontactscore.cloudcontactscore import CloudContactScore
-from shapedesign.src.utilities.pose import pose_cas_are_identical
+from cubicsym.utilities import pose_cas_are_identical
 from symmetryhandler.reference_kinematics import perturb_jumpdof_str_int, get_jumpdof_str_int, get_dofs, dof_str_to_int, set_jumpdof_str_int
-from shapedesign.src.utilities.pose import CA_rmsd_without_alignment
-from shapedesign.src.visualization.visualizer import get_visualizer_with_score_attached
-from shapedesign.src.utilities.score import create_sfxn_from_terms
 from cubicsym.actors.symdefswapper import SymDefSwapper
 import math
 from cubicsym.cubicsetup import CubicSetup
@@ -24,136 +21,136 @@ from cloudcontactscore.cloudcontactscorecontainer import CloudContactScoreContai
 from cubicsym.utilities import copy_pose_id_to_its_bases, add_id_to_pose_w_base
 import warnings
 
-class SymmetrySlider:
-    """A symmetric slider using CloudContactScore (CCS)"""
-    def __init__(self, pose, acceptable_connections=2, trans_mag=3, max_slide_attempts = 1000,
-                 visualizer=None, native=None, freeze_x_at=0, freeze_y_at=0, freeze_z_at=0, css_kwargs=None):
-        self.ccs = CloudContactScore(pose, **css_kwargs) if css_kwargs else CloudContactScore(pose, None)
-        self.x_params,  self.y_params, self.z_params = None, None, None
-        self.store_trans_params(pose) # FIXME: this is not going to work if they are spread over multiple jumps
-        self.trans_mag = trans_mag
-        self.max_slide_attemps = max_slide_attempts
-        self.visualizer = get_visualizer_with_score_attached(visualizer, self.ccs)
-        self.acceptable_connections = acceptable_connections
-        self.native = native
-        self.freeze_x_at = freeze_x_at
-        self.freeze_y_at = freeze_y_at
-        self.freeze_z_at = freeze_z_at
-        self.freeze_x = False
-        self.freeze_y = False
-        self.freeze_z = False
-
-    def x(self, pose, slide_dir):
-        if not self.freeze_x:
-            perturb_jumpdof_str_int(pose, *self.x_params, value = slide_dir * self.trans_mag)
-            if get_jumpdof_str_int(pose, *self.x_params) <= self.freeze_x_at:
-                perturb_jumpdof_str_int(pose, *self.z_params, value = (-slide_dir) * self.trans_mag)
-                self.freeze_x = True
-
-    def y(self, pose, slide_dir):
-        if not self.freeze_y:
-            perturb_jumpdof_str_int(pose, *self.y_params, value=slide_dir * self.trans_mag)
-            if get_jumpdof_str_int(pose, *self.y_params) <= self.freeze_y_at:
-                perturb_jumpdof_str_int(pose, *self.y_params, value=(-slide_dir) * self.trans_mag)
-                self.freeze_y = True
-
-    def z(self, pose, slide_dir):
-        if not self.freeze_z:
-            perturb_jumpdof_str_int(pose, *self.z_params, value = slide_dir * self.trans_mag)
-            if get_jumpdof_str_int(pose, *self.z_params) <= self.freeze_z_at:
-                perturb_jumpdof_str_int(pose, *self.z_params, value = (-slide_dir) * self.trans_mag)
-                self.freeze_z = True
-
-    def store_trans_params(self, pose):
-        trans_dofs = []
-        for jumpname, jumpparams in get_dofs(pose).items():
-            for dofname in jumpparams.keys():
-                if dofname == "x":
-                    self.x_params = (jumpname, dof_str_to_int[dofname])
-                elif dofname == "y":
-                    self.y_params = (jumpname, dof_str_to_int[dofname])
-                elif dofname == "z":
-                    self.z_params = (jumpname, dof_str_to_int[dofname])
-        return trans_dofs
-
-    def compute_clashes_and_connections(self, pose):
-        self.ccs.internal_update(pose)
-        return self.compute_clashes_only(), self.compute_acceptable_hf_interaction()
-
-    def compute_clashes_only(self):
-        return self.ccs.compute_clashes()
-
-    def compute_acceptable_hf_interaction(self):
-        self.hf_clashes = self.ccs.compute_hf_clashes()
-        return sum([v > 0 for k, v in self.hf_clashes.items()])
-
-    def get_moveset(self):
-        moveset = []
-        if not self.freeze_x and self.x_params:
-            moveset.append(self.x)
-        if not self.freeze_y and self.y_params:
-            moveset.append(self.y)
-        if not self.freeze_z and self.z_params:
-            moveset.append(self.z)
-        random.shuffle(moveset)
-        return moveset
-
-    def apply(self, pose, report=False):
-        if report:
-            t = time.time()
-            start_rmsd = CA_rmsd_without_alignment(pose, self.native)
-        n = 0
-        finish = FalsE
-        while not finish and n != self.max_slide_attemps:
-            n += 1
-            # get current clashes and connections
-            self.ccs.internal_update(pose)
-            current_clashes, current_connections = self.compute_clashes_and_connections(pose)
-            slide_dir = -1 # into the cubic center
-            # pick a random move from the moveset and check that it is allowed. If not, pick another moveset until a move has been allowed
-            # or all moves have been used. If not are allowed then a move is forced.
-            moveset = self.get_moveset()
-            if not moveset: # all moves are frozen!
-                break
-            clashes = []
-            for move in moveset:
-                move(pose, slide_dir)
-                move_clashes, move_connections = self.compute_clashes_and_connections(pose)
-                clashes.append(move_clashes)
-                # if we have all the connections we want -> Stop
-                if move_connections >= self.acceptable_connections:
-                    finish = True
-                    break
-                # if we lost a connection, go back and try to pick another move
-                elif move_connections < current_connections:
-                    move(pose, - slide_dir)
-                    continue
-                # if we introduced a clash, go back and try to pick another move
-                if move_clashes > current_clashes:
-                    move(pose, - slide_dir)
-                    continue
-                # move was allowed but the protocol is not finished yet because we do not have all the interactions yet
-                break
-            # all moves have been used and not all connections have been made and they all introduce clashes, then force the move that
-            # produces the lowest amount of clashes
-            if len(moveset) == len(clashes):
-                move = moveset[clashes.index(min(clashes))]
-                move(pose, slide_dir)
-                self.ccs.internal_update(pose)
-                move_connections = self.compute_acceptable_hf_interaction()
-                if move_connections == self.acceptable_connections:
-                    finish = True
-            if self.visualizer:
-                self.visualizer.send_pose(pose)
-        print(f"Connected {' and '.join([str(k) for k, v in self.hf_clashes.items() if v > 0]) }")
-        if report:
-            data = get_dofs(pose)
-            data["total_time(s)"] = time.time() - t
-            data["start_rmsd"] = start_rmsd
-            data["end_rmsd"] = CA_rmsd_without_alignment(pose, self.native)
-            data["slide_attempts"] = n
-            data["connections"] = '+'.join([str(k) for k, v in self.hf_clashes.items() if v > 0])
-            return data
+# class SymmetrySlider:
+#     """A symmetric slider using CloudContactScore (CCS)"""
+#     def __init__(self, pose, acceptable_connections=2, trans_mag=3, max_slide_attempts = 1000,
+#                  visualizer=None, native=None, freeze_x_at=0, freeze_y_at=0, freeze_z_at=0, css_kwargs=None):
+#         self.ccs = CloudContactScore(pose, **css_kwargs) if css_kwargs else CloudContactScore(pose, None)
+#         self.x_params,  self.y_params, self.z_params = None, None, None
+#         self.store_trans_params(pose) # FIXME: this is not going to work if they are spread over multiple jumps
+#         self.trans_mag = trans_mag
+#         self.max_slide_attemps = max_slide_attempts
+#         self.visualizer = get_visualizer_with_score_attached(visualizer, self.ccs)
+#         self.acceptable_connections = acceptable_connections
+#         self.native = native
+#         self.freeze_x_at = freeze_x_at
+#         self.freeze_y_at = freeze_y_at
+#         self.freeze_z_at = freeze_z_at
+#         self.freeze_x = False
+#         self.freeze_y = False
+#         self.freeze_z = False
+#
+#     def x(self, pose, slide_dir):
+#         if not self.freeze_x:
+#             perturb_jumpdof_str_int(pose, *self.x_params, value = slide_dir * self.trans_mag)
+#             if get_jumpdof_str_int(pose, *self.x_params) <= self.freeze_x_at:
+#                 perturb_jumpdof_str_int(pose, *self.z_params, value = (-slide_dir) * self.trans_mag)
+#                 self.freeze_x = True
+#
+#     def y(self, pose, slide_dir):
+#         if not self.freeze_y:
+#             perturb_jumpdof_str_int(pose, *self.y_params, value=slide_dir * self.trans_mag)
+#             if get_jumpdof_str_int(pose, *self.y_params) <= self.freeze_y_at:
+#                 perturb_jumpdof_str_int(pose, *self.y_params, value=(-slide_dir) * self.trans_mag)
+#                 self.freeze_y = True
+#
+#     def z(self, pose, slide_dir):
+#         if not self.freeze_z:
+#             perturb_jumpdof_str_int(pose, *self.z_params, value = slide_dir * self.trans_mag)
+#             if get_jumpdof_str_int(pose, *self.z_params) <= self.freeze_z_at:
+#                 perturb_jumpdof_str_int(pose, *self.z_params, value = (-slide_dir) * self.trans_mag)
+#                 self.freeze_z = True
+#
+#     def store_trans_params(self, pose):
+#         trans_dofs = []
+#         for jumpname, jumpparams in get_dofs(pose).items():
+#             for dofname in jumpparams.keys():
+#                 if dofname == "x":
+#                     self.x_params = (jumpname, dof_str_to_int[dofname])
+#                 elif dofname == "y":
+#                     self.y_params = (jumpname, dof_str_to_int[dofname])
+#                 elif dofname == "z":
+#                     self.z_params = (jumpname, dof_str_to_int[dofname])
+#         return trans_dofs
+#
+#     def compute_clashes_and_connections(self, pose):
+#         self.ccs.internal_update(pose)
+#         return self.compute_clashes_only(), self.compute_acceptable_hf_interaction()
+#
+#     def compute_clashes_only(self):
+#         return self.ccs.compute_clashes()
+#
+#     def compute_acceptable_hf_interaction(self):
+#         self.hf_clashes = self.ccs.compute_hf_clashes()
+#         return sum([v > 0 for k, v in self.hf_clashes.items()])
+#
+#     def get_moveset(self):
+#         moveset = []
+#         if not self.freeze_x and self.x_params:
+#             moveset.append(self.x)
+#         if not self.freeze_y and self.y_params:
+#             moveset.append(self.y)
+#         if not self.freeze_z and self.z_params:
+#             moveset.append(self.z)
+#         random.shuffle(moveset)
+#         return moveset
+#
+#     def apply(self, pose, report=False):
+#         if report:
+#             t = time.time()
+#             start_rmsd = CA_rmsd_without_alignment(pose, self.native)
+#         n = 0
+#         finish = FalsE
+#         while not finish and n != self.max_slide_attemps:
+#             n += 1
+#             # get current clashes and connections
+#             self.ccs.internal_update(pose)
+#             current_clashes, current_connections = self.compute_clashes_and_connections(pose)
+#             slide_dir = -1 # into the cubic center
+#             # pick a random move from the moveset and check that it is allowed. If not, pick another moveset until a move has been allowed
+#             # or all moves have been used. If not are allowed then a move is forced.
+#             moveset = self.get_moveset()
+#             if not moveset: # all moves are frozen!
+#                 break
+#             clashes = []
+#             for move in moveset:
+#                 move(pose, slide_dir)
+#                 move_clashes, move_connections = self.compute_clashes_and_connections(pose)
+#                 clashes.append(move_clashes)
+#                 # if we have all the connections we want -> Stop
+#                 if move_connections >= self.acceptable_connections:
+#                     finish = True
+#                     break
+#                 # if we lost a connection, go back and try to pick another move
+#                 elif move_connections < current_connections:
+#                     move(pose, - slide_dir)
+#                     continue
+#                 # if we introduced a clash, go back and try to pick another move
+#                 if move_clashes > current_clashes:
+#                     move(pose, - slide_dir)
+#                     continue
+#                 # move was allowed but the protocol is not finished yet because we do not have all the interactions yet
+#                 break
+#             # all moves have been used and not all connections have been made and they all introduce clashes, then force the move that
+#             # produces the lowest amount of clashes
+#             if len(moveset) == len(clashes):
+#                 move = moveset[clashes.index(min(clashes))]
+#                 move(pose, slide_dir)
+#                 self.ccs.internal_update(pose)
+#                 move_connections = self.compute_acceptable_hf_interaction()
+#                 if move_connections == self.acceptable_connections:
+#                     finish = True
+#             if self.visualizer:
+#                 self.visualizer.send_pose(pose)
+#         print(f"Connected {' and '.join([str(k) for k, v in self.hf_clashes.items() if v > 0]) }")
+#         if report:
+#             data = get_dofs(pose)
+#             data["total_time(s)"] = time.time() - t
+#             data["start_rmsd"] = start_rmsd
+#             data["end_rmsd"] = CA_rmsd_without_alignment(pose, self.native)
+#             data["slide_attempts"] = n
+#             data["connections"] = '+'.join([str(k) for k, v in self.hf_clashes.items() if v > 0])
+#             return data
 
 # todo: This should inherit from SymmmetrySlider
 class CubicSymmetrySlider:
